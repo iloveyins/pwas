5a73a6e61de7415f261f461077c749b3ca5ea062
yangyu <yu.yang@szcal.onaliyun.com>
2018-09-25 16:12:28 +0800

    content

--- src/app/details-test/details-test.component.ts
+++ src/app/details-test/details-test.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { IndexedDbService } from '../utils/IndexedDb.Service';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as Mock from 'mockjs';
 
 @Component({
   selector: 'app-details-test',
@@ -9,12 +11,14 @@ import { IndexedDbService } from '../utils/IndexedDb.Service';
   styleUrls: ['./details-test.component.css']
 })
 export class DetailsTestComponent implements OnInit {
-  title: string = "默认";
+  src: string="";
+  content:string="www.baidu.com"
   constructor(
     private http: HttpClient,
-    private server: IndexedDbService
+    private router: ActivatedRoute
+    // private server: IndexedDbService
   ) {
-     
+
   }
   myDB = {
     name: "test",
@@ -37,24 +41,35 @@ export class DetailsTestComponent implements OnInit {
   }];
 
   ngOnInit() {
-    this.server.openDB(this.myDB.name, this.myDB.version,this.myDB);
-    setTimeout(function () {
-      this.server.addData(this.myDB.db, 'students');
-      this.server.getDataByKey(this.myDB.db, 'students', 1002);
-      //updateDataByKey(myDB.db, 'students', student1);
-    }, 1000);
+    const data = Mock.mock({
+      'list|1-10':[{
+        'id|+1':1
+      }]
+    });
 
-    if (navigator.onLine) {
-      this.getData().subscribe((data) => {
-        this.title = data["items"][0].etag;
-      });
-    } else {
-      this.title = 'assets/offline.jepg';
-    }
+   var s =  Mock.mock('@paragraph');
+   this.content = s;
+    // this.server.openDB(this.myDB.name, this.myDB.version,this.myDB);
+    // setTimeout(function () {
+    //   this.server.addData(this.myDB.db, 'students');
+    //   this.server.getDataByKey(this.myDB.db, 'students', 1002);
+    //   //updateDataByKey(myDB.db, 'students', student1);
+    // }, 1000);
+    this.router.queryParams.subscribe(q => {
+       this.src = q.src;
+    });
+    // if (navigator.onLine) {
+    //   this.getData().subscribe((data) => {
+    //     this.title = data["items"][0].etag;
+    //   });
+    // } else {
+    //   //this.title = 'assets/offline.jepg';
+    // }
   }
 
   getData() {
-    return this.http.get(
-      "https://www.googleapis.com/books/v1/volumes/?q=science");
+    var googleurl ="https://www.googleapis.com/books/v1/volumes/?q=science";
+    var url ="http://suggest.taobao.com/sug?code=utf-8&q=零食&callback=cb";
+    return this.http.get(url);
   }
 }
